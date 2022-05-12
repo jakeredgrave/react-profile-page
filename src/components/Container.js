9056cd33ea9daaec348aa8c84c7bb6b28397a4ef
Jakub Staniek <jakub.staniek@icloud.com>
2022-05-12 14:05:46 +0200

    Fixed PostQuantity component

--- src/components/Container.js
+++ src/components/Container.js
@@ -5,7 +5,7 @@ import PostsQuantity from "./PostsQuantity";
 const Container = ({ profileInfo, posts, onShow, onDelete }) => {
   return (
     <div className="container grid">
-      <PostsQuantity profileInfo={profileInfo} />
+      <PostsQuantity profileInfo={profileInfo} posts={posts} />
       <Header profileInfo={profileInfo} onShow={onShow} />
       <Posts posts={posts} profileInfo={profileInfo} onDelete={onDelete} />
     </div>
